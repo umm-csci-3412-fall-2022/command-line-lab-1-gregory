eb3d421f38e5f6ae0a6031e86e83c3681d9530a6
Gregory Petterson <pet03356@morris.umn.edu>
2022-09-06 22:43:32 -0500

    Don't entirely understand gcc. What does wall do?  What is linking? I know that "./" means curent directory but why do we need this before executing NthPrime?

---
+++ compiling/extract_and_compile.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+num=$1
+
+tar -zxf NthPrime.tgz 
+cd NthPrime
+#Don't entirely understand gcc. What does wall do? What is linking?
+gcc -Wall main.c nth_prime.c -o NthPrime
+#I know that "./" means curent directory but why do we need this before executing NthPrime?
+./NthPrime $num
+
