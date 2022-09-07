72a99fc62a52c2589d75f155a3cf3570027226e3
Gregory Petterson <pet03356@morris.umn.edu>
2022-09-06 22:57:37 -0500

    All the tests work and I believe NthPrime is removed from my last run. I left questions in the script and in my last commit.

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -4,7 +4,7 @@ num=$1
 
 tar -zxf NthPrime.tgz 
 cd NthPrime
-#Don't entirely understand gcc. What does wall do? What is linking?
+#Don't entirely understand gcc. What does wall do? What is linking? Why dp we need to output it into another NthPrime directory?
 gcc -Wall main.c nth_prime.c -o NthPrime
 #I know that "./" means curent directory but why do we need this before executing NthPrime?
 ./NthPrime $num
