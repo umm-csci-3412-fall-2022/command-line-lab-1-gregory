a58f87aaaad760f9c0df61874bb4ad866140c3e1
Lloyd Hilsgen <hilsg008@tycho.morris.umn.edu>
2022-09-08 09:52:20 -0500

    Making the code Shellcheck compliant

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -3,9 +3,9 @@
 num=$1
 
 tar -zxf NthPrime.tgz 
-cd NthPrime
+cd NthPrime || exit
 #Don't entirely understand gcc. What does wall do? What is linking? Why dp we need to output it into another NthPrime directory?
 gcc -Wall main.c nth_prime.c -o NthPrime
 #I know that "./" means curent directory but why do we need this before executing NthPrime?
-./NthPrime $num
+./NthPrime "$num"
 
